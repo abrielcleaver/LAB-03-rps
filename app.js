b576fd4dc243543aafdae4ef780a377444dcfc61
Abriel Cleaver <abrielcleaver@gmail.com>
2021-10-06 16:23:32 -0700

    add working play again button

--- app.js
+++ app.js
@@ -7,40 +7,44 @@
   // use user input to update state 
   // update DOM to reflect the new state
 
-  import { didUserWin, getRandomThrow } from "./utils.js";
-  
-  const playButton = document.getElementById('play');
-  const winSpan = document.getElementById('win');
-  const lostSpan = document.getElementById('lost');
-  const drawSpan = document.getElementById('draw');
-  const result = document.getElementById('result');
+import { didUserWin, getRandomThrow } from "./utils.js";
 
-  let win = 0;
-  let lost = 0;
-  let draw = 0;
+const playButton = document.getElementById('play');
+const winSpan = document.getElementById('win');
+const lostSpan = document.getElementById('lost');
+const drawSpan = document.getElementById('draw');
+const result = document.getElementById('result');
+const againButton = document.getElementById('play-again');
+const msg1 = document.getElementById('msg1');
+const msg2 = document.getElementById('msg2');
+const msg3 = document.getElementById('msg3');
 
-  playButton.addEventListener('click', () => {
+let win = 0;
+let lost = 0;
+let draw = 0;
+
+playButton.addEventListener('click', () => {
   // console.log('click')
   
   const selected = document.querySelector('input[type=radio]:checked');
   if(!selected){
     return error.classList.remove('hidden');
   }
+  
   error.classList.add('hidden');
   const userChoice = selected.value;
   const computerThrow = getRandomThrow();
 
-  if (userChoice === computerThrow){
-    draw++;
-  }
   if (didUserWin(userChoice, computerThrow)){
     win++;
-    result.textContent = 'You Won! It was ${computerThrow}';
-  } else {
+    result.textContent = `You Won! It was ${computerThrow}`;
+  } else  {
     lost++;
-    result.textContent = 'You Lost! It was ${computerThrow}'; 
+    result.textContent = `You Lost! It was ${computerThrow}`; 
+  } 
+  if (didUserWin(userChoice === computerThrow)){
+    draw++;
   }
-
 // if the userChoice === computerThrow return 'draw'
 // if the user throws rock && computer throws scissors, return 'win'
 // else if the user throws paper && computer throws rock, return 'win'
@@ -50,4 +54,12 @@
   winSpan.textContent = win;
   lostSpan.textContent = lost;
   drawSpan.textContent = draw; 
+  });
+
+  againButton.addEventListener('click', () =>{
+    result.textContent = '' ;
+    msg1.textContent = 'You won 0 times';
+    msg2.textContent = 'You lost 0 times';
+    msg3.textContent = 'You had 0 draws'; 
+
   })
