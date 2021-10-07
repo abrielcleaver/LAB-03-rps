113d40ac90b0e4326776076c2a2c3aecadaec73b
Abriel Cleaver <abrielcleaver@gmail.com>
2021-10-06 19:18:06 -0700

    clean up layout of page

--- test/example.test.js
+++ test/example.test.js
@@ -1,14 +1,8 @@
-// IMPORT MODULES under test here:
-
-// import { example } from '../example.js';
-
 import { didUserWin } from '../utils.js';
 
 const test = QUnit.test;
 
 test('Draw - userChoice === computerThrow', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
     const expected = 'draw';
     const actual = didUserWin('rock', 'rock');
     expect.equal(actual,expected);
